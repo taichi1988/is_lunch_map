ed9bff08651bde89ea94cd425a866d9e87cbeddb
taichi1988 <r83if7zfj@gmail.com>
2018-08-05 22:04:21 +0900

    first commit

---
+++ is_lunch_map/is_lunch_mapUITests/is_lunch_mapUITests.swift
@@ -0,0 +1,36 @@
+//
+//  is_lunch_mapUITests.swift
+//  is_lunch_mapUITests
+//
+//  Created by 行木太一 on 2018/08/05.
+//  Copyright © 2018年 yukit.Inc. All rights reserved.
+//
+
+import XCTest
+
+class is_lunch_mapUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
