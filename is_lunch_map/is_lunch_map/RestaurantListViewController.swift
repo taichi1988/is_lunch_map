6478da2ee5786e121b3798236d1d62fba6cf4694
taichi1988 <r83if7zfj@gmail.com>
2018-08-05 22:34:08 +0900

    飲食店一覧画面クラス作成

---
+++ is_lunch_map/is_lunch_map/RestaurantListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantListViewController.swift
+//  is_lunch_map
+//
+//  Created by 行木太一 on 2018/08/05.
+//  Copyright © 2018年 yukit.Inc. All rights reserved.
+//
+
+import UIKit
+
+final class RestaurantListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orange
+    }
+}
